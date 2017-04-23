6b0ef0e61d55a21cefca3ce3ff7e1f29b6207738
alfonsodelavega <alfonsodelavegaruiz@gmail.com>
2017-04-23 18:15:13 +0200

    Language extraction from infrastructure repository

---
+++ es.unican.lavoisier.tests/src/es/unican/lavoisier/tests/TestLavoisier.xtend
@@ -0,0 +1,5 @@
+package es.unican.lavoisier.tests
+
+class TestLavoisier {
+
+}
---
+++ es.unican.lavoisier/src/es/unican/lavoisier/domainModelProvider/IDomainModelUser.java
@@ -0,0 +1,7 @@
+package es.unican.lavoisier.domainModelProvider;
+
+import org.eclipse.emf.ecore.EPackage;
+
+public interface IDomainModelUser {
+  public void setDomainModel(EPackage domainModel);
+}
